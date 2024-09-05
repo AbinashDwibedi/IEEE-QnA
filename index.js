fd84375a4354914763924da9e57c9dd913fad0c1
= <=>
2024-09-05 22:16:17 +0530

    Day 5 added

--- index.js
+++ index.js
@@ -172,3 +172,121 @@
 // }
 // console.log(convertTemp(25))
 // //output: 77
+
+// function longStr(str){
+//     let i , j , newStr = [];
+//     for(i=0;i<str.length ;i++){
+//         if(!newStr.includes(str[i])){
+//             newStr.push(str[i])
+//         }
+//         else{
+//             break;
+//         }
+//     }
+//     return newStr.join("");
+// }
+
+// function isIncluded(newstr , character){
+//     for(let i = 0 ;i< newstr.length ; i++){
+//         if(newstr[i] == character){
+//             return true
+//         }
+//     }
+//     return false
+// }
+
+// console.log(longStr("workattech"));
+
+
+// function longStr(str){
+//     let i , j , newStr = [];
+//     for(i=0;i<str.length ;i++){
+//         if(!isIncluded(newStr , str[i])){
+//             newStr.push(str[i])
+//         }
+//         else{
+//             break;
+//         }
+//     }
+//     return newStr.join("");
+// }
+
+// function isIncluded(newstr , character){
+//     for(let i = 0 ;i< newstr.length ; i++){
+//         if(newstr[i] == character){
+//             return true
+//         }
+//     }
+//     return false
+// }
+
+// console.log(longStr("workattech"));
+
+// function checkWays(num){
+//     let arr= [0,1] , value;
+//     while(num>0){
+//         value = arr[0]+arr[1];
+//         arr[0] = arr[1];
+//         arr[1] = value
+//         num--
+//     }
+//     return value;
+// }
+
+// console.log(checkWays(4))
+// //output : 5
+
+// function checkLPrime(L, R) {
+//     let primeNumbersArray = primeNumbers(L,R);
+//     if(primeNumbers.length ==0){
+//         return -1
+//     }
+//     let IndividualNumArray = [], countArr = [0,0], max;
+//     primeNumbersArray.forEach((number)=>{
+//         IndividualNumArray = IndividualNumArray.concat(breakNum(number));
+//     })
+//     IndividualNumArray.sort((a,b)=> a-b)
+    
+//     for(let i = IndividualNumArray[IndividualNumArray.length-1] ;i>0;i--){
+//         countArr[0] = 0;
+//         for(let j = 1; j<IndividualNumArray.length;j++){
+//             if(i==IndividualNumArray[j]){
+//                 countArr[0]++;
+//             }
+//         }
+//         if(countArr[0]>countArr[1]){
+//             countArr[1] = countArr[0]
+//             max = i
+//         }
+//     }
+//     return max;
+// }
+// function breakNum(num){
+//     let brokenNum = [];
+//     while(num>0){
+//         brokenNum.push(num%10);
+//         num = Number.parseInt(num/10);
+//     }
+//     return brokenNum
+// }
+// function primeNumbers(L,R){
+//     let i,j,count,numArr = [] ;
+//     for(i=L;i<=R ;i++){
+//         count = 0 ;
+//         for(j=1;j<=i;j++){
+//             if(i%j==0){
+//                 count++
+//             }
+//         }
+//         if(count ==2){
+//             numArr.push(i)
+//         }
+//     }
+//     return numArr
+// }
+// console.log(checkLPrime(2,20));
+// //output : 1
+
+
+
+
